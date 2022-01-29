8459bc4345076f360d964efa14b2c7e0e188681b
George Desktop <georgecaws@gmail.com>
2022-01-29 17:07:24 +0000

    renamed and modified to work

--- firebase_cache.py
+++ firebase_cache.py
@@ -1,17 +1,11 @@
 from firebase_admin import credentials
 from firebase_admin import firestore
 import firebase_admin
+from spotipy import CacheHandler
 
-class FirestoreCacheHandler():
-    def __init__(self, user, project_id):
-        cred = credentials.ApplicationDefault()
-        
-        firebase_admin.initialize_app(cred, {
-            'projectId': project_id
-        })
-        
-        db = firestore.client()
-        self.doc_ref = db.collection('users').document(user)
+class FirestoreCacheHandler(CacheHandler):
+    def __init__(self, doc_ref):
+        self.doc_ref = doc_ref
     
     def get_cached_token(self):
         return(self.doc_ref.get(field_paths = ['token']).to_dict()['token'])
